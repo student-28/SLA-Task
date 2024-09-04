4b451abd33468ae4008413c0ce3784689eaeb268
Nandhagopal S <110347484+student-28@users.noreply.github.com>
2024-09-04 16:45:43 +0530

    Create Task-4(p1).js

---
+++ Task-4(p1).js
@@ -0,0 +1,13 @@
+// write a program to print even and odd numbers using for loop between 1 to 20
+console.log("EVEN NUMBERS BETWEEN 1 TO 20 \n");
+for(let i=1;i<=20;i++){
+  if(i%2==0){
+    console.log(i);
+  }
+}
+console.log("\nODD NUMBERS BETWEEN 1 TO 20\n");
+for(let i=1;i<=20;i++){
+  if(i%2!=0){
+    console.log(i);
+  }
+}
