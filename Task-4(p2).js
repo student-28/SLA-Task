1ab01bb4139136edd5c2bfeb8690bceb9904140a
Nandhagopal S <110347484+student-28@users.noreply.github.com>
2024-09-04 17:02:04 +0530

    Create Task-4(p2).js

---
+++ Task-4(p2).js
@@ -0,0 +1,27 @@
+// write a program to print all weekdays and weekdays using switch case
+let day=5;
+switch(day){
+  case 1:
+    console.log("SUNDAY");
+    break;
+  case 2:
+    console.log("MONDAY");
+    break;
+  case 3:
+    console.log("TUESDAY");
+    break;
+  case 4:
+    console.log("WEDNESDAY");
+    break;
+  case 5:
+    console.log("THURSDAY");
+    break;
+  case 6:
+    console.log("FRIDAY");
+    break;
+  case 7:
+    console.log("SATURDAY");
+    break;
+  default:
+    console.log("Enter a valid day number");
+}
