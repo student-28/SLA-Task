d2983ad7fd3ce86e67d92f168e4c2aa33f65df0b
Nandhagopal S <110347484+student-28@users.noreply.github.com>
2024-09-23 20:04:57 +0530

    Update AddUsingStatic.java

--- JavaTask/Task2/AddUsingStatic.java
+++ JavaTask/Task2/AddUsingStatic.java
@@ -1,9 +1,11 @@
-
 public class AddUsingStatic {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 static int add(int a,int b) {
+		return a+b;
 	}
-
+     public static void main(String[] args) {
+		int a=10;
+		int b=20;
+	    System.out.println(add(a,b));
+	
+	}
 }
